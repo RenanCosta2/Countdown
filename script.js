d1641ca27571a8d71907c1551107e5ddbe5a101a
RenanCosta2 <105220100+RenanCosta2@users.noreply.github.com>
2022-08-03 20:05:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,64 @@
 let timer = document.querySelectorAll('.countdown div')
 let num = document.querySelectorAll('.time-design p')
 
-let inicio = 86400000
-let fim = 86400000 * 15
+let inicio = new Date().getTime()
+let fim = new Date('08/03/2022').getTime()
 
 let distance = fim - inicio
+    
+let y = setInterval(() => {
+    
+    
+    if(distance <= 0){
+    
+        distance = fim - inicio
+        fim += 86400000 * 14
+    
+    } else{
 
-console.log(distance)
-
-window.addEventListener('load', ()=>{
-
-    let x = setInterval(function() {
-
-        if(distance > 0){
-            let days = Math.floor(distance / (1000 * 60 * 60 * 24))
-            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-            let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000)
     
-            if(days < 10){
+        if(days < 10){
     
-                num[0].textContent = `0`+ days
+            num[0].textContent = `0`+ days
     
-            } else{
-                num[0].textContent = days
-            }
-    
-            if(hours < 10){
-    
-                num[1].textContent = `0`+ hours
-    
-            } else{
-                num[1].textContent = hours
-            }
-    
-            if(minutes < 10){
-    
-                num[2].textContent = `0`+ minutes
-    
-            } else{
-                num[2].textContent = minutes
-            }
-    
-            if(seconds < 10){
-    
-                num[3].textContent = `0`+ seconds
-    
-            } else{
-                num[3].textContent = seconds
-            }
+        } else{
+            num[0].textContent = days
         }
     
-    }, 1000);
+        if(hours < 10){
     
-    let y = setInterval(() => {
-        distance = distance - 1000
+            num[1].textContent = `0`+ hours
     
-        if(distance <= 0){
-    
-            distance = fim - inicio
-    
+        } else{
+            num[1].textContent = hours
         }
     
-    }, 1000);
+        if(minutes < 10){
     
-})
+            num[2].textContent = `0`+ minutes
+    
+        } else{
+            num[2].textContent = minutes
+        }
+    
+        if(seconds < 10){
+    
+            num[3].textContent = `0`+ seconds
+    
+        } else{
+            num[3].textContent = seconds
+        }
+
+    }
+
+    distance = distance - 1000
+    
+}, 1000);
+    
 
 
 
