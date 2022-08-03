f9ba6aa2e78a2e0b1a488de7776bf053f37ea556
RenanCosta2 <105220100+RenanCosta2@users.noreply.github.com>
2022-08-03 18:52:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let timer = document.querySelectorAll('.countdown div')
 let num = document.querySelectorAll('.time-design p')
 
-let inicio = new Date("08/01/2022 00:00:00").getTime()
-let fim = new Date('08/15/2022 00:00:00').getTime()
+let inicio = 86400000
+let fim = 86400000 * 15
 
 let distance = fim - inicio
+
+console.log(distance)
+
+window.addEventListener('load', ()=>{
 
     let x = setInterval(function() {
 
@@ -13,50 +17,54 @@ let distance = fim - inicio
             let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
             let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
             let seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
+    
             if(days < 10){
-
+    
                 num[0].textContent = `0`+ days
-
+    
             } else{
                 num[0].textContent = days
             }
-
+    
             if(hours < 10){
-
+    
                 num[1].textContent = `0`+ hours
-
+    
             } else{
                 num[1].textContent = hours
             }
-
+    
             if(minutes < 10){
-
+    
                 num[2].textContent = `0`+ minutes
-
+    
             } else{
                 num[2].textContent = minutes
             }
-
+    
             if(seconds < 10){
-
+    
                 num[3].textContent = `0`+ seconds
-
+    
             } else{
                 num[3].textContent = seconds
             }
         }
-
+    
     }, 1000);
-
+    
     let y = setInterval(() => {
         distance = distance - 1000
-
+    
         if(distance <= 0){
-
+    
             distance = fim - inicio
-
+    
         }
-
+    
     }, 1000);
+    
+})
+
+
 
